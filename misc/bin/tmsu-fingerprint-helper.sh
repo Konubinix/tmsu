30f23b07454bad17a15c8c08ccabe7e10b1b03f8
Samuel Loury <konubinixweb@gmail.com>
2013-12-21 12:39:38 +0100

    If tmsu-fingerprint-helper does not know how to handle the file, it asks to fall back to the internal

--- misc/bin/tmsu-fingerprint-helper.sh
+++ misc/bin/tmsu-fingerprint-helper.sh
@@ -3,5 +3,9 @@
 while read file
 do
 	FILE_PATH="$(basename $(readlink "${file}"))"
+	if [ "$?" != "0" ]
+	then
+		FILE_PATH=internal
+	fi
 	echo $FILE_PATH|sed 's/^.\+--\([^.]\+\)\(\..\+\)\?$/\1/'
 done
